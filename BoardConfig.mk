99d72bbddc0bef516691ede77d662d87f8639c89
Yukino1314520 <1475215798@qq.com>
2025-12-14 21:37:41 +0800

    daumier: Inherit generic_ramdisk product configuration

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,6 +5,9 @@
 
 DEVICE_PATH := device/xiaomi/daumier
 
+# Inherit generic_ramdisk product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
+
 # A/B
 AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS += \
