5d3979f8fe3371642b3720e4602179aa4e6c228a
Yukino1314520 <1475215798@qq.com>
2025-12-14 21:12:44 +0800

    daumier: Define Architecture Flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,5 +5,11 @@
 
 DEVICE_PATH := device/xiaomi/daumier
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv9-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_VARIANT := cortex-a76
+
 # Inherit the proprietary files
 include vendor/xiaomi/daumier/BoardConfigVendor.mk
