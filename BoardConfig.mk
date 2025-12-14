eff8076a81d155cf4ac4d589fcaa14b663b5ad1f
Yukino1314520 <1475215798@qq.com>
2025-12-14 21:13:58 +0800

    daumier: Define Bootloader and Platform flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,5 +11,12 @@ TARGET_ARCH_VARIANT := armv9-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_VARIANT := cortex-a76
 
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := daumier
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6983
+
 # Inherit the proprietary files
 include vendor/xiaomi/daumier/BoardConfigVendor.mk
