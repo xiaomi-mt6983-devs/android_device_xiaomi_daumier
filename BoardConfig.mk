cc2e8a6633a66a3eac7909ea64ad8472c0193d2b
Yukino1314520 <1475215798@qq.com>
2025-12-15 21:06:40 +0800

    daumier: Inherit common MTK and Xiaomi FCMs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -78,6 +78,11 @@ TARGET_SCREEN_DENSITY := 411
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     vendor/lineage/config/device_framework_matrix.xml
+
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    vendor/lineage/config/device_framework_matrix.xml \
+    hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
     
 # Kernel
 KERNEL_PREBUILTS_DIR := $(DEVICE_PATH)-kernel
