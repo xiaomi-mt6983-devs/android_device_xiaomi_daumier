e2caa2735cfe08e59ebb00ae537b93b2a5607e88
Yukino1314520 <1475215798@qq.com>
2025-12-16 14:26:59 +0800

    daumier: Patch pq-impl for forked utils

--- extract-files.py
+++ extract-files.py
@@ -28,7 +28,10 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
-
+    'vendor/lib64/hw/mt6895/vendor.mediatek.hardware.pq@2.15-impl.so': blob_fixup()
+    .replace_needed('libutils.so', 'libutils-v32.so')
+    .replace_needed('libsensorndkbridge.so', 'android.hardware.sensors@1.0-convert-shared.so'),
+    
 }  # fmt: skip
 
 module = ExtractUtilsModule(
