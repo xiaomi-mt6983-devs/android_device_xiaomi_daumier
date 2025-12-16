ade6319564321991171ca9f2f490727b78a87117
Yukino1314520 <1475215798@qq.com>
2025-12-16 20:20:21 +0800

    daumier: Configure Sensors stack

--- device.mk
+++ device.mk
@@ -215,6 +215,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/init.recovery.mt6983.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.mt6983.rc
 
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@2.0-subhal-impl-1.0 \
+    android.hardware.sensors-service.xiaomi-multihal
+
+PRODUCT_PACKAGES += \
+    sensors.dynamic_sensor_hal
+    
 # Shipping API levels
 PRODUCT_SHIPPING_API_LEVEL := 31
 
