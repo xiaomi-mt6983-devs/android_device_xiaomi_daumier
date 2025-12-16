a416ff9177a7958ba6955402201564ecf99e97e5
Yukino1314520 <1475215798@qq.com>
2025-12-16 14:21:41 +0800

    daumier: Add device FCM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,9 +81,10 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     vendor/lineage/config/device_framework_matrix.xml \
+    $(DEVICE_PATH)/framework_compatibility_matrix.xml \
     hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
-    
+
 # Kernel
 KERNEL_PREBUILTS_DIR := $(DEVICE_PATH)-kernel
 
