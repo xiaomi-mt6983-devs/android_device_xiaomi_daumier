7eeb9a40b88de7491faa4953cced1fc77eb59897
Yukino1314520 <1475215798@qq.com>
2025-12-16 17:24:51 +0800

    daumier: Remove leading tabs from txpowerctrl.cfg
    * Significantly reduces the size of the file and subsequently
      the required size of the config buffer in gen4m.
    
    * Should hopefully also allow "pure" gen4m to recognise
      and load the configs without Xiaomi's hacks (which
      they didn't release).

--- extract-files.py
+++ extract-files.py
@@ -34,6 +34,9 @@ blob_fixups: blob_fixups_user_type = {
     .replace_needed('android.hardware.security.secureclock-V1-ndk_platform.so', 'android.hardware.security.secureclock-V1-ndk.so')
     .add_needed('android.hardware.security.rkp-V3-ndk.so'),
 
+    'vendor/firmware/txpowerctrl.cfg': blob_fixup()
+        .regex_replace('\t', ''),
+
     'vendor/lib64/hw/mt6983/vendor.mediatek.hardware.pq@2.15-impl.so': blob_fixup()
     .replace_needed('libutils.so', 'libutils-v32.so')
     .replace_needed('libsensorndkbridge.so', 'android.hardware.sensors@1.0-convert-shared.so'),
