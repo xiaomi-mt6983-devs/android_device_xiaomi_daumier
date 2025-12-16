2d91e1f16d7904e96fccdd4f31c062501cf8f114
Yukino1314520 <1475215798@qq.com>
2025-12-16 20:36:06 +0800

    daumier: Configure Vibrator stack
    * Build the common QTI Vibrator service since there is support for
      aw8697_haptic input device.

--- device.mk
+++ device.mk
@@ -128,7 +128,7 @@ PRODUCT_PACKAGES += \
 # Light
 PRODUCT_PACKAGES += \
     android.hardware.light-service.lineage
-    
+
 # Modules
 PRODUCT_PACKAGES += \
     init.insmod.sh \
@@ -243,6 +243,15 @@ PRODUCT_PACKAGES += \
 # Vendor service manager
 PRODUCT_PACKAGES += \
     vndservicemanager
+
+# Vibrator
+$(call soong_config_set, vibrator, vibratortargets, vibratoraidlV2target)
+
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service
+
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/daumier/daumier-vendor.mk)
