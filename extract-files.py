b640858b6882be5f635e57b52b0258f50b7faf54
Yukino <1475215798@qq.com>
2025-12-16 22:59:03 +0800

    daumier: patch libmnl to libcutils

--- extract-files.py
+++ extract-files.py
@@ -61,6 +61,9 @@ blob_fixups: blob_fixups_user_type = {
         .clear_symbol_version('AHardwareBuffer_unlock')
         .add_needed('libbase_shim.so'),
 
+    'vendor/lib64/mt6983/libmnl.so': blob_fixup()
+        .add_needed('libcutils.so'),
+
     'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b': blob_fixup()
         .add_needed('libstagefright_foundation-v33.so')
         .add_needed('libprocessgroup.so')
