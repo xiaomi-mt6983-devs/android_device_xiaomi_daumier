c2f731e25409f394f866ca5104498d79c7e9d761
Yukino <1475215798@qq.com>
2025-12-16 22:52:50 +0800

    daumier: patch mtkfusionrild to v32

--- extract-files.py
+++ extract-files.py
@@ -37,6 +37,9 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/firmware/txpowerctrl.cfg': blob_fixup()
         .regex_replace('\t', ''),
 
+    'vendor/bin/hw/mtkfusionrild': blob_fixup()
+        .add_needed('libutils-v32.so'),
+        
     'vendor/lib64/hw/mt6983/vendor.mediatek.hardware.pq@2.15-impl.so': blob_fixup()
     .replace_needed('libutils.so', 'libutils-v32.so')
     .replace_needed('libsensorndkbridge.so', 'android.hardware.sensors@1.0-convert-shared.so'),
