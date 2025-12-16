e6f000b81344ff86be60b51701dae7fac27bc31c
Yukino <1475215798@qq.com>
2025-12-17 00:34:10 +0800

    daumier:  Drop miss init.conninfra.rc

--- device.mk
+++ device.mk
@@ -209,7 +209,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.mt6983 \
     fstab.mt6983.vendor_ramdisk \
-    init_conninfra.rc \
     init_aee.rc \
     init.batterysecret.rc \
     init.cgroup.rc \
